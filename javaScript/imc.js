a93578a7dd7c23d1281eaf52dacd62318dfe05f5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-24 21:21:51 -0300

    Armazenando todos os campos em variáveis.

---
+++ javaScript/imc.js
@@ -0,0 +1,4 @@
+var campoNome = document.getElementById("nome");
+var campoIdade = document.getElementById("idade");
+var campoPeso = document.getElementById("peso");
+var campoAltura = document.getElementById("altura");
