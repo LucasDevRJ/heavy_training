b345e2d04e6e1eef3255cb2a844a8bc0184da6bf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-24 21:42:22 -0300

    Função para calcular IMC.

--- javaScript/imc.js
+++ javaScript/imc.js
@@ -1,9 +1,20 @@
-var campoNome = document.getElementById("nome");
-var campoIdade = document.getElementById("idade");
-var campoPeso = document.getElementById("peso");
-var campoAltura = document.getElementById("altura");
+function calculaImc() {
+	var campoNome = document.getElementById("nome");
+	var campoIdade = document.getElementById("idade");
+	var campoPeso = document.getElementById("peso");
+	var campoAltura = document.getElementById("altura");
 
-var nome = campoNome.value;
-var idade = campoIdade.value;
-var peso = campoPeso.value;
-var altura = campoAltura.value;
+	var nome = campoNome.value;
+	var idade = campoIdade.value;
+	var peso = campoPeso.value;
+	var altura = campoAltura.value;
+
+	idade = parseInt(idade);
+	peso = parseFloat(peso);
+	altura = parseFloat(altura);
+
+	var imc = peso / (altura * altura);
+}
+
+var botao = document.getElementById("botao");
+botao.addEventListener("click", calculaImc);
