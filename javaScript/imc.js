7810d775880434ccffcabbbb7d2ed7b85c9eadc2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-24 21:23:07 -0300

    Armazenando os valores dos inputs

--- javaScript/imc.js
+++ javaScript/imc.js
@@ -2,3 +2,8 @@ var campoNome = document.getElementById("nome");
 var campoIdade = document.getElementById("idade");
 var campoPeso = document.getElementById("peso");
 var campoAltura = document.getElementById("altura");
+
+var nome = campoNome.value;
+var idade = campoIdade.value;
+var peso = campoPeso.value;
+var altura = campoAltura.value;
