cee7abb3e62ab7d9bceed1d90f57e04b35cbec57
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-25 21:20:33 -0300

    Construção do gráfico.

--- javaScript/imc.js
+++ javaScript/imc.js
@@ -22,6 +22,7 @@ function desenhaGrafico(imc) {
 	var grafico = document.getElementById("grafico");
 	var pincel = grafico.getContext("2d");
 
+	pincel.fillStyle = "black";
 	pincel.font = "12px Arial";
 	pincel.fillText("Menor que 18,5", 5, 50);
 	pincel.fillStyle = "red";
@@ -31,6 +32,11 @@ function desenhaGrafico(imc) {
 	pincel.fillText("18,5 a 24,9", 120, 50);
 	pincel.fillStyle = "green";
 	pincel.fillRect(100, 0, 100, 30);
+
+	pincel.fillStyle = "black";
+	pincel.fillText("25 a 29,9", 220, 50);
+	pincel.fillStyle = "red";
+	pincel.fillRect(200, 0, 100, 30);
 }
 
 var botao = document.getElementById("botao");
