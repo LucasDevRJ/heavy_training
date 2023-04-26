53bceaffb1f4a1acba956f978491bb7312dd1208
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-25 21:30:30 -0300

    Gráficos contruídos

--- javaScript/imc.js
+++ javaScript/imc.js
@@ -27,16 +27,43 @@ function desenhaGrafico(imc) {
 	pincel.fillText("Menor que 18,5", 5, 50);
 	pincel.fillStyle = "red";
 	pincel.fillRect(0, 0, 100, 30);
+	pincel.strokeStyle = "black";
+	pincel.strokeRect(0, 0, 100, 30);
 
 	pincel.fillStyle = "black";
 	pincel.fillText("18,5 a 24,9", 120, 50);
 	pincel.fillStyle = "green";
 	pincel.fillRect(100, 0, 100, 30);
+	pincel.strokeStyle = "black";
+	pincel.strokeRect(100, 0, 100, 30);
 
 	pincel.fillStyle = "black";
 	pincel.fillText("25 a 29,9", 220, 50);
 	pincel.fillStyle = "red";
 	pincel.fillRect(200, 0, 100, 30);
+	pincel.strokeStyle = "black";
+	pincel.strokeRect(200, 0, 100, 30);
+
+	pincel.fillStyle = "black";
+	pincel.fillText("30 a 34,9", 320, 50);
+	pincel.fillStyle = "red";
+	pincel.fillRect(300, 0, 100, 30);
+	pincel.strokeStyle = "black";
+	pincel.strokeRect(300, 0, 100, 30);
+
+	pincel.fillStyle = "black";
+	pincel.fillText("35 a 39,9", 420, 50);
+	pincel.fillStyle = "red";
+	pincel.fillRect(400, 0, 100, 30);
+	pincel.strokeStyle = "black";
+	pincel.strokeRect(400, 0, 100, 30);
+
+	pincel.fillStyle = "black";
+	pincel.fillText("Maior que 40", 520, 50);
+	pincel.fillStyle = "red";
+	pincel.fillRect(500, 0, 100, 30);
+	pincel.strokeStyle = "black";
+	pincel.strokeRect(500, 0, 100, 30);
 }
 
 var botao = document.getElementById("botao");
